5983c3894a82314eaf2ee6ff487665f8ccf85ab5
NMRayhan <nurmdrayhan97@gmail.com>
2022-03-26 02:45:38 +0600

    All Component are Created

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Product.css";
 
 const Product = (props) => {
     const { name, color, Price, model, Image } = props.car
@@ -11,8 +12,8 @@ const Product = (props) => {
                     <h5 className='card-text fw-lighter'>Model : <span className='muted'>{model}</span></h5>
                     <h6 className='card-text fw-lighter'>Color : <span className="text-muted">{color}</span></h6>
                     <div className='row justify-content-center align-items-center'>
-                        <div className='col-6'>
-                            <h6>Price :{Price}</h6>
+                        <div className='col-6 price'>
+                            <h6>Price : ${Price}M</h6>
                         </div>
                         <div className='col-6'>
                             <button className='btn btn-secondary'>Add Cart</button>
